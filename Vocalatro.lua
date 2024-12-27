7efb7037c980bae6cb7846db204ce2e244058105
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2024-12-27 18:02:23 +0700

    initial commit

---
+++ Vocalatro.lua
@@ -0,0 +1,5 @@
+
+    if SMODS.Atlas then
+        SMODS.Atlas {key = 'collab_WF_1', path = "collabs/collab_WF_1.png",atlas_table = 'ASSET_ATLAS',prefix_config = { key = false },px=71,py=95}
+-- SMODS.Atlas({name = 'collab_WF_2', path = "textures/"..self.SETTINGS.GRAPHICS.texture_scaling.."x/collabs/collab_WF_2.png",px=71,py=95})
+end
