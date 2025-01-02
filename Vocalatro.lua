dcb2d8fcb986192cb72cbe38f45d4dff67444c83
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2025-01-03 01:38:07 +0700

    neru temp

--- Vocalatro.lua
+++ Vocalatro.lua
@@ -1,9 +1,91 @@
-
 ranks = {"Jack", "Queen", "King"}
-SMODS.Atlas {key = 'vocalatro_clubs_1', path = "collabs/mikugumizunda.png",atlas_table = 'ASSET_ATLAS',prefix_config = { key = false },px=71,py=95}
-SMODS.Atlas {key = 'vocalatro_clubs_2', path = "collabs/mikugumizunda_cb.png",atlas_table = 'ASSET_ATLAS',prefix_config = { key = false },px=71,py=95}
-SMODS.Atlas {key = 'vocalatro_hearts_1', path = "collabs/tetoreiia.png",atlas_table = 'ASSET_ATLAS',prefix_config = { key = false },px=71,py=95}
-SMODS.Atlas {key = 'vocalatro_hearts_2', path = "collabs/tetoreiia_cb.png",atlas_table = 'ASSET_ATLAS',prefix_config = { key = false },px=71,py=95}
+SMODS.Atlas {
+    key = 'vocalatro_clubs_1',
+    path = "collabs/mikugumizunda.png",
+    atlas_table = 'ASSET_ATLAS',
+    prefix_config = {
+        key = false
+    },
+    px = 71,
+    py = 95
+}
+SMODS.Atlas {
+    key = 'vocalatro_clubs_2',
+    path = "collabs/mikugumizunda_cb.png",
+    atlas_table = 'ASSET_ATLAS',
+    prefix_config = {
+        key = false
+    },
+    px = 71,
+    py = 95
+}
+SMODS.Atlas {
+    key = 'vocalatro_hearts_1',
+    path = "collabs/tetoreiia.png",
+    atlas_table = 'ASSET_ATLAS',
+    prefix_config = {
+        key = false
+    },
+    px = 71,
+    py = 95
+}
+SMODS.Atlas {
+    key = 'vocalatro_hearts_2',
+    path = "collabs/tetoreiia_cb.png",
+    atlas_table = 'ASSET_ATLAS',
+    prefix_config = {
+        key = false
+    },
+    px = 71,
+    py = 95
+}
+SMODS.Atlas {
+    key = 'vocalatro_diamonds_1',
+    path = "collabs/nerurinlenakari.png",
+    atlas_table = 'ASSET_ATLAS',
+    prefix_config = {
+        key = false
+    },
+    px = 71,
+    py = 95
+}
 
-SMODS.DeckSkin({key = 'vocalatro_clubs', suit="Clubs", ranks=ranks, lc_atlas='vocalatro_clubs_1',hc_atlas="vocalatro_clubs_2",posStyle = 'collab',loc_txt={["en-us"]="Miku, Gumi, & Zundamon"}})
-SMODS.DeckSkin({key = 'vocalatro_hearts', suit="Hearts", ranks=ranks, lc_atlas='vocalatro_hearts_1',hc_atlas="vocalatro_hearts_2",posStyle = 'collab',loc_txt={["en-us"]="Teto, Rei, & IA"}})
+SMODS.DeckSkin({
+    key = 'vocalatro_clubs',
+    suit = "Clubs",
+    ranks = ranks,
+    lc_atlas = 'vocalatro_clubs_1',
+    hc_atlas = "vocalatro_clubs_2",
+    posStyle = 'collab',
+    loc_txt = {
+        ["en-us"] = "Miku, Gumi, & Zundamon",
+        ["ja"] = "ミク, メグッポイド, ずんだもん",
+        ["th_TH"] = "มิกุ, กุมิ, และซีนดาม่อน"
+    }
+})
+SMODS.DeckSkin({
+    key = 'vocalatro_hearts',
+    suit = "Hearts",
+    ranks = ranks,
+    lc_atlas = 'vocalatro_hearts_1',
+    hc_atlas = "vocalatro_hearts_2",
+    posStyle = 'collab',
+    loc_txt = {
+        ["en-us"] = "Teto, Rei, & IA",
+        ["ja"] = "テト, レイ, & イア",
+        ["th_TH"] = "เทโตะ, เร, และไออะ"
+    }
+})
+SMODS.DeckSkin({
+    key = 'vocalatro_diamonds',
+    suit = "Diamonds",
+    ranks = ranks,
+    lc_atlas = 'vocalatro_diamonds_1',
+    hc_atlas = "vocalatro_diamonds_1",
+    posStyle = 'collab',
+    loc_txt = {
+        ["en-us"] = "Neru, Rin/Len, & Akari",
+        ["ja"] = "ネル, リン/レン, & あかり",
+        ["th_TH"] = "เนรุ, ริน/เรน, และอะคะริ"
+    }
+})
